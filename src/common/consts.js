69632dee13752a9f737b34688ac21a8399d1ac8c
Abdu Jemal <abdu1994jemal@gmail.com>
2025-07-06 00:59:40 +0300

    api url changed

--- src/common/consts.js
+++ src/common/consts.js
@@ -1,9 +1,9 @@
 import { useLocation } from "react-router-dom";
 
 // urls
-// export const mainUrl = "https://api.ilmfelagi.com/api/v1"
+export const mainUrl = "https://api.ilmfelagi.com/api/v1"
 // export const mainUrl = "https://www.ilmfelagi.com/api/v1";
-export const mainUrl = "http://localhost:3000/api/v1";
+// export const mainUrl = "http://localhost:3000/api/v1";
 
 export const courseUrl = `${mainUrl}/courses`;
 export const searchCourseUrl = `${mainUrl}/courses/search`;
